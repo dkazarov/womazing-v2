d3ee0a98653b0c8e850f45f614deda69a617608e
unknown <d.kazarov1361@gmail.com>
2024-11-23 17:27:24 +0200

    hero: connect sluder swiper

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,4 +1,54 @@
 import { burgerOpen, burgerClose } from './js-components/burger';
 
+import Swiper from 'swiper';
+import { Pagination, Autoplay } from 'swiper/modules';
+
+const heroSliderLeft = () => {
+	new Swiper('.hero-slider__left', {
+		modules: [Pagination, Autoplay],
+
+		loop: true,
+
+		autoplay: {
+			delay: 2000,
+		},
+
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+};
+
+const heroSliderRight = () => {
+	new Swiper('.hero-slider__right', {
+		modules: [Pagination, Autoplay],
+
+		loop: true,
+
+		autoplay: {
+			delay: 2000,
+		},
+
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+	});
+};
+
 burgerOpen();
 burgerClose();
+
+heroSliderLeft();
+heroSliderRight();
